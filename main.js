2f9548a6135762d86b7fdc164e46bc50fc46b988
Ashley78 <86760458+Ashley78@users.noreply.github.com>
2021-08-14 09:26:45 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+Webcam.set({
+    width:320,
+    height:320,
+    image_format:'png',
+    png_quality:90,
+
+    constraints:{
+        facingMode:"environment"
+    }
+});
+camera=document.getElementById("cam");
+Webcam.attach('#cam');
+
+function take_snapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML='<img id="captured_image" src="'+data_uri+'"/>';
+    })
+}
+console.log('ml5 version:', ml5.version);
+
+classifier=ml5.imageClassifier('MobileNet',model_loaded);
+
+function model_loaded(){
+    console.log("model is loaded");
+}
+
+function check(){
+    img=document.getElementById('captured_image');
+    classifier.classify(img,gotResult);
+}
+function gotResult(error,results){
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        document.getElementById("object_name").innerHTML=results[0].label;
+    }
+}
